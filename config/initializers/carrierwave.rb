7502112b0d1cab6ea3cb6f25bc79698475dd3c61
Daniel Retta <dretta@ymail.com>
2015-03-31 02:10:50 -0700

    Added user photos

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ if Rails.env.production?
 	    config.fog_credentials = {    
 	    	# Configuration for Amazon S3   
 			:provider => 'AWS',
-			:aws_access_key_id => ENV['S3_ACCESS_KEY'],
-			:aws_secret_access_key => ENV['S3_SECRET_KEY'],
+			:aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+			:aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
 			:region => 'us-west-2',
 			:host => 's3.example.com',
 			:endpoint => 'https://s3.example.com:8080'
