37bee7b0612064945735edfdf64761cdda718997
Daniel Retta <dretta@ymail.com>
2015-03-31 01:59:24 -0700

    Added user photos

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
 	CarrierWave.configure do |config|
 		config.root = Rails.root.join('tmp') # adding these...
 		config.cache_dir = 'carrierwave' # ...two lines
-	    config.fog_credentials = {  
+	    config.fog_credentials = {    
 	    	# Configuration for Amazon S3   
 			:provider => 'AWS',
 			:aws_access_key_id => ENV['S3_ACCESS_KEY'],
