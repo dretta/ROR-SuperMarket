0fdfb304277ceb232ec90488841d9921fcb3713b
Daniel Retta <dretta@ymail.com>
2015-03-31 00:58:33 -0700

    Added user photos

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,18 @@
 if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
-    }
-    config.fog_directory     =  ENV['S3_BUCKET']
-  end
+	CarrierWave.configure do |config|
+		config.root = Rails.root.join('tmp') # adding these...
+		config.cache_dir = 'carrierwave' # ...two lines
+	    config.fog_credentials = {
+	    	# Configuration for Amazon S3
+			:provider => 'AWS',
+			:aws_access_key_id => ENV['S3_ACCESS_KEY'],
+			:aws_secret_access_key => ENV['S3_SECRET_KEY'],
+			:region => 'us-west-2',
+			:host => 's3.example.com',
+			:endpoint => 'https://s3.example.com:8080'
+		}
+		config.fog_directory     =  ENV['S3_BUCKET']
+		config.fog_public     = false # optional, defaults to true
+		config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+	end
 end
