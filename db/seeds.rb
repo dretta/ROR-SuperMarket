92bf6719df22a00a24a60ff16f71f7460e01c175
Daniel Retta <dretta@ymail.com>
2015-04-27 12:56:34 -0700

    Attempting to fix items_index_test.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,7 @@ User.create!(
 	admin: true,
 	activated: true,
 	activated_at: Time.zone.now,
-	picture: "admin.png"
-	shoppingcart: Shoppingcart.new)
+	picture: "admin.png")
 
 99.times do |n|
 	name  = Faker::Name.name
