d45f84b71810427ebeaaec84a2f71b874d5ae5a0
Daniel Retta <dretta@ymail.com>
2015-05-28 19:42:49 -0700

    Attempting to fix items_index_test.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -65,6 +65,7 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 
+
 	test "authenticated? should return false for a user with nil digest" do
 		assert_not @user.authenticated?(:remember, '')
   	end
